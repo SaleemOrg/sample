e97d82ed5690c69453a24ce2b86f470e9c358ddc
mohds <mohds@DESKTOP-A4S2IDB>
2020-11-17 00:43:10 +0530

    modified user1

--- sample/src/sample/LLIST.java
+++ sample/src/sample/LLIST.java
@@ -95,3 +95,64 @@ class Employee{
 	
 	
 }
+
+class Employee2{
+	@Override
+	public String toString() {
+		return "Employee [rollno=" + rollno + ", name=" + name + "]";
+	}
+
+	int rollno;
+	public int getRollno() {
+		return rollno;
+	}
+
+	public void setRollno(int rollno) {
+		this.rollno = rollno;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	String name;
+	
+	Employee2(int roll, String nm) {
+		rollno = roll;
+		name = nm;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		//result = prime * result + rollno;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Employee other = (Employee) obj;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+//		if (rollno != other.rollno)
+//			return false;
+		return true;
+	}
+	
+	
+}
